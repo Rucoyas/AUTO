b8e43859b4b37daeb87f9792fb7a53ad1ff752b8
Developer of Zassho Gilgamesh <korisawarezero@gmail.com>
2024-07-18 21:44:13 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -25,28 +25,38 @@ module.exports.run = async function({
       let page = 1;
       let start = (page - 1) * pages;
       let end = start + pages;
-      let helpMessage = `Command List:\n\n`;
+      let helpMessage = `~ 𝗠𝗬 𝗔𝗩𝗔𝗜𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧:\n\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
+        helpMessage += `\╭─❍
+➠ ${i + 1}.  ${prefix}${commands[i]} 
+╰───────────⟡ \n`;
       }
-      helpMessage += '\nEvent List:\n\n';
+      helpMessage += '\n~ 𝗠𝗬 𝗘𝗩𝗘𝗡𝗧 𝗟𝗜𝗦𝗧:\n\n';
       eventCommands.forEach((eventCommand, index) => {
-        helpMessage += `\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
+        helpMessage += `\╭─❍
+➠ ${index + 1}.  ${prefix}${eventCommand} 
+╰───────────⟡ \n`;
       });
-      helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the next page, type '${prefix}help page number'. To view information about a specific command, type '${prefix}help command name'.`;
+      helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the next page, type '${prefix}help page number'. To view information about a specific command, type '${prefix}help command name'
+
+𝗧𝗛𝗜𝗦 𝗕𝗢𝗧 𝗜𝗦 𝗨𝗦𝗜𝗡𝗚 𝗧𝗛𝗜𝗦 𝗔𝗨𝗧𝗢𝗕𝗢𝗧:https://gilgameshautobotv2.onrender.com`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
     } else if (!isNaN(input)) {
       const page = parseInt(input);
       const pages = 20;
       let start = (page - 1) * pages;
       let end = start + pages;
-      let helpMessage = `Command List:\n\n`;
+      let helpMessage = `Command list\n\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
+        helpMessage += `\╭─❍
+➠ ${i + 1}.  ${prefix}${commands[i]} 
+╰───────────⟡ \n`;
       }
-      helpMessage += '\nEvent List:\n\n';
+      helpMessage += '\nEvent list:\n\n';
       eventCommands.forEach((eventCommand, index) => {
-        helpMessage += `\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
+        helpMessage += `\╭─❍
+➠ ${index + 1}.  ${prefix}${eventCommand} 
+╰───────────⟡ \n`;
       });
       helpMessage += `\nPage ${page} of ${Math.ceil(commands.length / pages)}`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
@@ -95,4 +105,4 @@ module.exports.handleEvent = async function({
   if (body?.toLowerCase().startsWith('prefix')) {
     api.sendMessage(message, threadID, messageID);
   }
-}
+        }
